78bd655ecb7a207436265c75c0fa4b3d85f27f4f
Sean Omlor <somlor@eml.cc>
2018-07-18 14:28:32 -0400

    chapter 3

---
+++ typedefs.hs
@@ -0,0 +1,33 @@
+-- 3.11
+bools :: [Bool]
+bools = [True, False]
+
+nums :: [[Int]]
+nums = [[1, 3], [5, 7]]
+
+add :: Int -> Int -> Int -> Int
+add x y z = x + y + z
+
+copy :: a -> (a, a)
+copy x = (x, x)
+
+apply :: (a -> b) -> a -> b
+apply f x = f x
+
+second :: [a] -> a
+second xs = head (tail xs)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+double :: Num a => a -> a
+double x = x * 2
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
+
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
